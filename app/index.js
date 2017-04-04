355cd86c3fc2ff53daa96816588554c970ce2ee8
TerenYeung <yangjialong@ifanr.com>
2017-04-05 00:18:38 +0800

    将路由控制代码进行封装成staticFunc

--- app/index.js
+++ app/index.js
@@ -9,21 +9,15 @@ class App {
 			//获取request对象的url和method
 			let { url , method } = request;
 			//如果url以.action结尾，认为它是ajax;
-
-			//路由控制
-			if(url == '/css/index.css'){
-				fs.readFile('./public/css/index.css','utf8',(err,data)=>{
-					response.end(data);
-				})
-			}else if(url == '/js/index.js'){
-				fs.readFile('./public/js/index.js','utf8',(err,data)=>{
-					response.end(data);
-				})
-			}else if(url == '/'){
-				fs.readFile('./public/index.html','utf8',(err,data)=>{
+			let staticFunc = (url)=>{
+				if(url == '/'){
+					url = '/index.html';
+				}
+				fs.readFile(`./public/${url}`,'utf8',(err,data)=>{
 					response.end(data);
 				})
 			}
+			staticFunc(url);
 		}
 	}
 }
