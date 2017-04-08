94cbb2e6a84ae879c3afea13ef9c45707d29d9e3
TerenYeung <yangjialong@ifanr.com>
2017-04-08 21:44:42 +0800

    commit上次没commit的

---
+++ app/url-parser/index.js
@@ -0,0 +1,36 @@
+/*
+ * urlParser
+ * 处理客户端数据
+ */
+
+ // request: query + body + method
+
+ module.exports = (request)=>{
+
+ 	let { method, url, context } = request;
+
+ 	method = method.toLowerCase();
+
+	return Promise.resolve({
+		then: (resolve,reject)=>{
+			context.method = method;
+			context.query = {};
+			if(method == 'post'){
+				let data = '';
+				request.on('data', (chunk)=>{
+					data +=chunk;
+					// console.log(data);
+				}).on('end',()=>{
+
+					context.body = JSON.parse(data)
+
+					resolve();
+				});
+			}else {
+				resolve();
+			}
+
+		}
+	})
+
+ }
